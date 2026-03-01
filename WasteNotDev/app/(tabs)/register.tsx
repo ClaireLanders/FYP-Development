4c38c2316abbc4d53ce3620ea2698a69cb31602f
ClaireLanders <122327886@umail.ucc.ie>
2026-03-01 14:27:24 +0000

    user story 11 and 12 - add the tab and nav

---
+++ WasteNotDev/app/(tabs)/register.tsx
@@ -0,0 +1,10 @@
+// Organisation Registration tab screen (US11 & US12)
+// Renders the OrgRegistrationForm component for setting up new organisations
+import { StyleSheet } from 'react-native';
+import { OrgRegistrationForm } from '@/components/Registration/OrgRegistrationForm';
+
+export default function RegisterScreen() {
+  return <OrgRegistrationForm />;
+}
+
+const styles = StyleSheet.create({});
